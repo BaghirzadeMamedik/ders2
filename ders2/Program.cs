06b0c7c2316f6e0f8e4f5516ddf0bd5147d99fda
BaghirzadeMamedik <brizeforge@gmail.com>
2022-01-07 15:11:03 +0400

    ders2

---
+++ ders2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var ad = "Murad";
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(ad);
+//}
+
+var reqem = 5;
+
+var reqem1 = 0;
+//while (reqem != reqem1)
+//{
+//    Console.WriteLine(ad);
+//    reqem1++;
+//}
+
+string[] telebeler = {
+    "Murad","Vahid", "Aytac", "gunel,", "Vahid", "Aytac", "gunel","Vahid", "Aytac", "gunel"
+};
+for (int i = 0; i < telebeler.Length; i++)
+{
+    Console.WriteLine(telebeler[i]);
+}
+
